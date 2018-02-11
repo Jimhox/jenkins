ee4c916fd21a5e6db5117f8b6718799b6d3e0086
root <root@localhost.localdomain>
2018-02-11 11:18:45 -0500

    s3 backend

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -3,10 +3,17 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "terraform-jami"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 module "create_infra" {
   source = "../../modules/module"
   vpc_cidr_block = "172.16.0.0/16"
-  subnet_cidr_block =  "172.16.10.0/24"
+  subnet_cidr_block =  "172.16.12.0/24"
 }
+
